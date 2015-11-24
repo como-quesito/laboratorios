741ad439b386ae28c65d31f7a014e9d6f83cbf4e
como-quesito <rapidclimate@outlook.com>
2015-11-24 11:03:58 -0600

    Actualizacion 24 nov 2015

--- src/main/java/org/unitec/ControladorPrincipal.java
+++ src/main/java/org/unitec/ControladorPrincipal.java
@@ -1,7 +1,10 @@
 package org.unitec;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by campitos on 6/11/15.
@@ -13,5 +16,13 @@ public class ControladorPrincipal {
     @RequestMapping("/")
     public String  inicio(){
         return "inicio.html";
+    }
+
+    @CrossOrigin
+    @RequestMapping(value="/test", method= RequestMethod.GET, headers={"Accept=text/html"})
+    @ResponseBody
+    String hola(){
+
+        return "Hola mundo desde Cross origin";
     }
 }
--- src/main/resources/static/js/inicio.js
+++ src/main/resources/static/js/inicio.js
@@ -44,5 +44,125 @@ angular.module('inicio',['ngRoute','ngResource','ngFileUpload'])
        .controller('apartados',function($rootScope,$scope,Upload, $timeout,$rootScope, $http,$resource,$log) {
     $scope.hola = "hola desde los reactivos";
     console.log('Controlador apartados');
+
+
+}).controller('profesores',function($rootScope,$scope,Upload, $timeout,$rootScope, $http,$resource,$log){
+    $scope.hola="hola desde los reactivos";
+
+
+
+
+    var Profesor=$resource('reactivo/:id',{id:'@id'},{crear:{method:'POST'},
+        actualizar:{method:'PUT'}, borrar:{method:'DELETE'}});
+
+
+    /*
+
+     $rootScope.setSelected = function(prop){
+     $rootScope.selectedprop = prop;
+     console.log(prop.label);
+     };
+     $rootScope.props = [{label: "1.", tema:"Introduccion"},{label: "2.", tema:"bases"},{label: "3.", tema:"conclusiones"}];
+
+     $rootScope.agarrar=function(){
+
+     }
+
+
+     //GET Todos
+
+     $scope.reactivo=Reactivo.query(function(){
+     console.log($scope.reactivo.length);
+     })
+
+
+     //Get por id
+     $scope.buscarPorId=function(){
+
+     $scope.usu=Usuario.get({id:1
+
+     }, function(){
+     console.log("Usuario obtenido:"+$scope.usu.nombre);
+     });
+     }
+
+
+     //UPDATE
+     $scope.actualizarUsuario=function(){
+     console.log("antes del evento update");
+     var usuario = new Usuario();
+     usuario.login = $scope.login;
+     usuario.password = $scope.password;
+     usuario.email =$scope.email;
+     usuario.$actualizar(function (data) {
+     console.log(data.nombre);
+     });
+     }
+
+     //DELETE
+     $scope.borrarUsuario=function(){
+     var usuario=new Usuario();
+     usuario.id=5;
+     usuario.$borrar();
+     console.log("si se borro");
+     }
+     */
+    //POST
+    $scope.guardarProfesorReactivo=function() {
+
+
+
+
+
+        console.log("tamaño de los radiesitos:"+ $scope.misradiesitos.length);
+
+        var profesor = new Profesor({
+            'password': $rootScope.passwordactual,
+            'reactivos':[
+                {
+                    'pregunta':$scope.pregunta,
+                    'urlimagen':"leer-imagen/"+$rootScope.rutaimagen,
+                    'tema':$scope.tema,
+                    'retroalimentacion':$scope.retroalimentacion,
+                    'opciones':[
+                        {
+                            'titulo':$scope.titulo1,
+                            'acierto':valores[0]
+                        },
+                        {
+                            'titulo':$scope.titulo2,
+                            'acierto':valores[1]
+                        },
+                        {
+                            'titulo':$scope.titulo3,
+                            'acierto':valores[2]
+                        },
+                        {
+                            'titulo':$scope.titulo4,
+                            'acierto':valores[3]
+                        }
+                    ]
+                }
+            ]
+        });
+        //  profesor.password = $rootScope.passwordactual;
+
+
+
+
+        profesor.$crear(function (data) {
+            console.log(data.password);
+            console.log(data.reactivos[0].pregunta);
+            $rootScope.rutaimagen='';
+        });
+
+
+
+
+
+
+    };// Termina boton guardarUsuario()
+
+
 });
 
