657c044e8b05aa2d129992ac1e41de21019144a7
Elizabeth <elmarinp@yahoo.com>
2016-03-27 17:30:31 -0600

    con poio

--- src/main/java/org/unitec/ControladorApartados.java
+++ src/main/java/org/unitec/ControladorApartados.java
@@ -1,12 +1,16 @@
 package org.unitec;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.poi.ss.usermodel.*;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.Date;
 
 /**
@@ -26,4 +30,31 @@ Date date=new Date(fecha);
 
         return "La fecha es "+date2.getDayOfMonth();
     }
+
+    @RequestMapping(value="/apartado/{clave}", method= RequestMethod.GET, headers={"Accept=text/html"})
+    @ResponseBody String actualizar(@PathVariable String clave)throws Exception{
+        InputStream inp = new FileInputStream("C:\\Users\\Elizabeth\\Documents\\prueba.xlsx");
+        //InputStream inp = new FileInputStream("workbook.xlsx");
+
+        //InputStream inp = new FileInputStream("workbook.xlsx");
+
+        Workbook wb = WorkbookFactory.create(inp);
+        Sheet sheet = wb.getSheetAt(0);
+        Row row = sheet.getRow(1);
+        Cell cell = row.getCell(1);
+        if (cell == null)cell = row.createCell(2);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(clave);
+
+        //
+        FileOutputStream fileOut = new FileOutputStream("C:\\Users\\Elizabeth\\Documents\\prueba.xlsx");
+        wb.write(fileOut);
+        fileOut.close();
+
+
+        return "La  clave se actualizo a ;"+clave;
+    }
+
+
+
 }
--- src/main/java/org/unitec/LaboratoriosApplication.java
+++ src/main/java/org/unitec/LaboratoriosApplication.java
@@ -25,24 +25,7 @@ public class LaboratoriosApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(LaboratoriosApplication.class, args);
 
-        InputStream inp = new FileInputStream("/home/campitos/Dropbox/prueba.xlsx");
-        //InputStream inp = new FileInputStream("workbook.xlsx");
 
-        //InputStream inp = new FileInputStream("workbook.xlsx");
-
-        Workbook wb = WorkbookFactory.create(inp);
-        Sheet sheet = wb.getSheetAt(0);
-        Row row = sheet.getRow(1);
-        Cell cell = row.getCell(2);
-        if (cell == null)
-            cell = row.createCell(3);
-        cell.setCellType(Cell.CELL_TYPE_STRING);
-        cell.setCellValue("pruebita");
-
-        //
-        FileOutputStream fileOut = new FileOutputStream("/home/campitos/Dropbox/prueba.xlsx");
-        wb.write(fileOut);
-        fileOut.close();
 
 
 
